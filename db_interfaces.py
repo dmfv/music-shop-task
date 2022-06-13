f732aa1fe3fb0418681de9351f16414193162af9
dmfv <56220925+dmfv@users.noreply.github.com>
2022-06-13 13:02:24 +0200

    added function to get all record's names for band

--- db_interfaces.py
+++ db_interfaces.py
@@ -31,6 +31,40 @@ def get_band_songs_number(band_name, engine_connect):
     return songs_number
 
 # функция позволяющая выводить название всех компакт-дисков заданного ансамбля
+def get_band_music_records_names(band_name, engine_connect):
+    band_id     = get_band_id(band_name, engine_connect)
+    # get all songs from band
+    band_songs = connect.execute(songs.select().where(songs.c.band_id == band_id)).fetchall()        
+    if len(band_songs) == 0: # check if songs existing
+        return []
+
+    # get list of all song's ids from this band
+    band_songs_ids = []
+    for song in band_songs:
+        if song: # if non zeros array
+            band_songs_ids.append(song[0])
+    if len(band_songs_ids) == 0:
+        return []
+    
+    # get all records id with band's songs
+    music_records_band_songs_ids = set() # set because two (or more) records can have the same songs
+    for song_id in band_songs_ids:
+        # get all records with one song song_id
+        record_with_band_song = connect.execute(music_records_songs.select().where(music_records_songs.c.song_id == song_id)).fetchall() # get all records with band songs
+        for record in record_with_band_song:
+            if record: # if record not empty list
+                music_records_band_songs_ids.add(record[1]) # music_record_id - is second column
+    if len(music_records_band_songs_ids) == 0: # if no records existing with songs of this band
+        return []
+
+    name_of_band_records = []
+    for record_id in music_records_band_songs_ids:
+        band_records_names = connect.execute(music_records.select().where(music_records.c.music_record_id == record_id)).fetchall()         # get all songs from band
+        if band_records_names: # if not empty list
+            name_of_band_records.append(band_records_names[0][1])
+
+    return name_of_band_records
+
 
 # функция позволяющая показать лидеров продаж текущего года, то есть названия
 # компакт-дисков, которые чаще всего покупали в текущем
